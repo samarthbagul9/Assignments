6e57cbfedfa3b070d32fec5e4a604146cbe71dac
Samarth Bagul <samarth.bagul.cmfeb26@gmail.com>
2026-03-26 16:16:08 +0530

    Add files via upload

---
+++ OOPJ Assignments/Java Assignment 1/Question1.java
@@ -0,0 +1,26 @@
+/* Write a Java program to declare variables of all primitive data types and print their 
+values.  */ 
+
+class Question1 {
+    public static void main(String args[]) {
+
+        byte a = 100;
+        short b = 20000;
+        int c = 'A';
+        long d = 68487486L;
+        float e = 12345.67f;
+        boolean f = true;
+        double g = 54684484;
+        char h = 'M';
+
+        System.out.println("Primitive Datatypes:- ");
+        System.out.println("Byte Datatype:- " + a);
+        System.out.println("Short Datatype:- " + b);
+        System.out.println("Int Datatype:- " + c);
+        System.out.println("Long Datatype:- " + d);
+        System.out.println("Float Datatype:- " + e);
+        System.out.println("Boolean Datatype:- " + f);
+        System.out.println("Double Datatype:- " + g);
+        System.out.println("Char Datatype:- " + h);
+    }
+}
