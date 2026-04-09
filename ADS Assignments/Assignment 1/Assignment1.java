d6cbfd680a7a47960b708d71038518bd706db654
Samarth Bagul <samarth.bagul.cmfeb26@gmail.com>
2026-04-09 23:58:28 +0530

    Add files via upload

---
+++ ADS Assignments/Assignment 1/Assignment1.java
@@ -0,0 +1,14 @@
+/* 1. Factorial with Edge Case Handling 
+Problem Description: 
+You are given a non-negative integer n. Write a recursive function to compute its factorial. 
+The factorial of a number n is defined as: 
+ n! = n × (n-1) × (n-2) × ... × 1  
+ By definition, 0! = 1  
+Your function must correctly handle the edge case when n = 0. Also consider what should 
+happen if a negative number is passed. 
+Requirements: 
+ Use recursion only (no loops).  
+ Clearly define the base case.  
+ Ensure your function does not cause infinite recursion  */
+
+class Assignment1
